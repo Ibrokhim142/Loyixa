2fa5a5ee697ac724266f38b0846b8a4d4df7b15d
Macbook Air <macbookair@MacBook-Air-Macbook.local>
2024-07-20 11:47:52 +0500

    first commit

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='w-full h-screen flex justify-center items-center'><Link to="/Auth"><Auth/> </Link></div>
   )
 }
 
